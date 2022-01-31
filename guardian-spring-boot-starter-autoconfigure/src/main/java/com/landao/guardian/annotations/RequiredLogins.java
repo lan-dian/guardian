9097249c59666656f6fcd55b6d856d20d8b79026
langar <2642299437@qq.com>
2022-01-31 22:58:10 +0800

    添加readme

--- guardian-spring-boot-starter-autoconfigure/src/main/java/com/landao/guardian/annotations/RequiredLogins.java
+++ guardian-spring-boot-starter-autoconfigure/src/main/java/com/landao/guardian/annotations/RequiredLogins.java
@@ -5,11 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 标识这个变量是用户id,可以从工具类中直接获取
- */
-@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface UserId {
+@Target({ElementType.METHOD,ElementType.TYPE})
+public @interface RequiredLogins {
+
+    RequiredLogin[] requiredLogins() default {};
 
 }
